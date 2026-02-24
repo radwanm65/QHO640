7928463fd608dfd2e90707153606507b88825b9a
Dr Marwan Radwan <67072159+radwanm65@users.noreply.github.com>
2026-02-24 22:09:18 +0000

    Delete Week 6 directory

--- Week 6/Hitastic/src/main.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./components/app.jsx";
-import "bootstrap/dist/css/bootstrap.css";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
